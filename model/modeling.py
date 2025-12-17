0983ace2b05660cad10a68ef10d094700ac460eb
nssnveban <130351726+nssnveban@users.noreply.github.com>
2025-12-17 14:00:06 +0800

    Update and rename vitcross_seg_modeling.py to modeling.py

--- model/modeling.py
+++ model/modeling.py
@@ -17,8 +17,8 @@ import numpy as np
 from torch.nn import CrossEntropyLoss, Dropout, Softmax, Linear, Conv2d, LayerNorm
 from torch.nn.modules.utils import _pair
 from scipy import ndimage
-import model.vit_seg_configs as configs
-from model.vit_seg_modeling_resnet_skip import FuseResNetV2
+import model.configs as configs
+from model.resnet_skip import FuseResNetV2
 
 
 logger = logging.getLogger(__name__)
