a5c9d7f318c14c070b6ffefd0e924b27ab846dd0
nssnveban <130351726+nssnveban@users.noreply.github.com>
2026-03-06 11:13:01 +0800

    Update configs.py

--- model/configs.py
+++ model/configs.py
@@ -1,5 +1,29 @@
 import ml_collections
 
+def get_b16_config():
+    """Returns the ViT-B/16 configuration."""
+    config = ml_collections.ConfigDict()
+    config.patches = ml_collections.ConfigDict({'size': (16, 16)})
+    config.hidden_size = 768
+    config.transformer = ml_collections.ConfigDict()
+    config.transformer.mlp_dim = 3072
+    config.transformer.num_heads = 12
+    config.transformer.num_layers = 12
+    config.transformer.attention_dropout_rate = 0.0
+    config.transformer.dropout_rate = 0.1
+
+    config.classifier = 'seg'
+    config.representation_size = None
+    config.resnet_pretrained_path = None
+    config.pretrained_path = '../model/vit_checkpoint/imagenet21k/ViT-B_16.npz'
+    config.patch_size = 16
+    config.n_skip = 0
+
+    config.decoder_channels = (256, 128, 64, 16)
+    config.n_classes = 2
+    config.activation = 'softmax'
+    return config
+
 def get_testing():
     """Returns a minimal configuration for testing."""
     config = ml_collections.ConfigDict()
@@ -32,3 +56,4 @@ def get_r50_b16_config():
     config.activation = 'softmax'
 
     return config
+
